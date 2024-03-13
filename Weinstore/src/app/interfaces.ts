846949ac9b8ebe144179610b2778a9fe455d22a1
Zimmermann, Pierre <Pierre.Zimmermann@telekom.de>
2024-03-13 11:03:18 +0100

    Expanded interfaces

--- Weinstore/src/app/interfaces.ts
+++ Weinstore/src/app/interfaces.ts
@@ -4,12 +4,32 @@ export interface User {
     password: string
   }
 
-  export interface Wine {
-    name: string;
-    type: string;
-    origin: string;
-    region: string;
-    price: number;
-    rating: number;
+  export interface Wine_lager {
+    Produktnummer: string;
+    Bezeichnung: string;
+    Jahrgang: string;
+    Region: string;
+    Land: string;
+    Bestand: number;
+    Lagerort: string;
+    Verkaufspreis: string;
+    Einkaufspreis: string;
+  }
+
+  export interface Wine_versand {
+    Produktnummer: string;
+    Bezeichnung: string;
+    Lagerort: string;
+    Verkaufspreis: string;
+  }
+
+  export interface Wine_CS {
+    Produktnummer: string;
+    Bezeichnung: string;
+    Jahrgang: string;
+    Region: string;
+    Land: string;
+    Bestand: number;
+    Verkaufspreis: string;
   }
   
